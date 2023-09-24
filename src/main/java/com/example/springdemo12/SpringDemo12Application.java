1832ab2ec794ab5dd9293a027da1ada5719c25e0
unknown <keertinaikwad@gmain.com>
2023-09-25 00:22:12 +0530

    first-commit

---
+++ src/main/java/com/example/springdemo12/SpringDemo12Application.java
@@ -0,0 +1,14 @@
+package com.example.springdemo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo12Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(SpringDemo12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdemo12/SpringDemo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemo12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
